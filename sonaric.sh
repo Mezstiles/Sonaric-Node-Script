d2fc7664fc9609e5724945c8d0dce72ed879823e
Zenith <161211651+dxzenith@users.noreply.github.com>
2024-07-14 13:03:00 +0530

    Create sonaric.sh

---
+++ sonaric.sh
@@ -0,0 +1,9 @@
+## VPS Requirements
+
+|                | Minimal                  | Recommended                  |
+|----------------|--------------------------|------------------------------|
+| **RAM**        | 4 GB RAM                 | 8 GB+ RAM                    |
+| **CPU Cores**  | 2 CPU cores              | 4+ CPU cores                 |
+| **Disk Space** | 20 GB free disk space    | 100 GB+ free disk space (SSD)|
+| **Architecture** | amd64 CPU Architecture | amd64 CPU Architecture       |
+| **Operating System** | Ubuntu 22.04       | Ubuntu 22.04                 |
