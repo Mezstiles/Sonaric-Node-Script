9a3a9d37dae0c129ca2620b553e854b06402eda5
Zenith <161211651+dxzenith@users.noreply.github.com>
2024-07-17 02:56:10 +0530

    Update sonaric-v1.5.0.sh

--- sonaric-v1.5.0.sh
+++ sonaric-v1.5.0.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+export RED='\033[0;31m'
+export GREEN='\033[0;32m'
+export YELLOW='\033[1;33m'
+export NC='\033[0m'  # No Color
+
+prompt() {
+    local message="$1"
+    read -p "$message" input
+    echo "$input"
+}
+
+execute_and_prompt() {
+    local message="$1"
+    local command="$2"
+    echo -e "${YELLOW}${message}${NC}"
+    eval "$command"
+    echo -e "${GREEN}Done.${NC}"
+}
+
+echo
+execute_and_prompt "Stopping sonaric node..." "sudo systemctl stop sonaricd"
+echo
+execute_and_prompt "Updating packaging lists..." "sudo apt update"
+echo
+execute_and_prompt "Upgrading sonaric node..." "sudo apt upgrade sonaric"
+echo
+execute_and_prompt "Reloading systemd manager config..." "sudo systemctl daemon-reload"
+echo
+execute_and_prompt "Restarting sonaric node..." "sudo systemctl restart sonaricd"
+echo
+execute_and_prompt "Checking sonaric node status..." "sudo systemctl status sonaricd"
+echo
