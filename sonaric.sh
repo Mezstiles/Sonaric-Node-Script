4b3fcf8eafe725239508eef3a1c7e5c072e1d5fb
Zenith <161211651+dxzenith@users.noreply.github.com>
2024-07-17 21:22:41 +0530

    Update sonaric.sh

--- sonaric.sh
+++ sonaric.sh
@@ -3,7 +3,7 @@
 export RED='\033[0;31m'
 export GREEN='\033[0;32m'
 export YELLOW='\033[1;33m'
-export NC='\033[0m'  # No Color
+export NC='\033[0m'
 
 prompt() {
     local message="$1"
@@ -19,30 +19,6 @@ execute_and_prompt() {
     echo -e "${GREEN}Done.${NC}"
 }
 
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-echo
-execute_and_prompt "Updating and upgrading packages..." "sudo apt update -y && sudo apt upgrade -y"
-echo
-execute_and_prompt "Removing Node.js..." "sudo apt-get remove -y nodejs"
-echo
-execute_and_prompt "Installing NVM and Node.js LTS..." 'curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash && \
-    export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" && \
-    nvm install --lts && nvm use --lts && \
-    echo -e "${GREEN}Node.js installed: $(node -v)${NC}"'
-
-sleep 2
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-sleep 2
-
-execute_and_prompt "Updating npm to the latest version..." "npm install -g npm@latest"
 echo
 execute_and_prompt "Installing Sonaric..." 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/monk-io/sonaric-install/main/linux-install-sonaric.sh)"'
 echo
